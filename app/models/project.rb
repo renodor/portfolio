74e6463b10711a6e73075b0f45186f13aae05b51
renaud dor <renaud.dor@gmail.com>
2020-03-13 14:34:21 +0100

    adding cloudinary

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+  has_one_attached :cover_photo
+  has_many_attached :photos
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'projects#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:show]
+  resources :projects, only: [:show, :new, :create]
 end
