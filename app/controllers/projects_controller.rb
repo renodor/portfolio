8b106ddb8cb3bde35899823da21f118ce5d3f457
renaud dor <renaud.dor@gmail.com>
2020-03-13 18:35:22 +0100

    working on projects grid

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,8 +1,8 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
-    @odd_projects = Project.where('id % 2 != 0')
-    @even_projects = Project.where('id % 2 = 0')
+    @odd_projects = @projects.select { |project| @projects.index(project).even? }
+    @even_projects = @projects.select { |project| @projects.index(project).odd? }
   end
 
   def show
