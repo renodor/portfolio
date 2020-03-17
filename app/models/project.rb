c72a481552000d102b665be9c3d39edf092c3eab
renaud dor <renaud.dor@gmail.com>
2020-03-17 17:42:48 +0100

    test projects images (without cloudinary), add services section and working on front and responsive

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  validates :name, :cover_photo, presence: true
+  validates :name, presence: true
   has_many_attached :photos
   has_one_attached :cover_photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+p "Destroy all Projects"
+Project.destroy_all
+p "---------> Done!"
+p ""
+p "Create New Projects"
+
+Project.create!(name: "Aliance Francaise", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.", url: "www.exemple.com")
+
+Project.create!(name: "Projet Source", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.", url: "www.exemple.com")
+
+Project.create!(name: "Line Up Trade", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.", url: "www.exemple.com")
+
+Project.create!(name: "OhLaLa Panama", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.", url: "www.exemple.com")
+
+Project.create!(name: "Salvamimaquina", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.", url: "www.exemple.com")
+
+Project.create!(name: "Bd Troc", description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.", url: "www.exemple.com")
+
+p "---------> Done!"
+p "#{Project.count} projects created!"
