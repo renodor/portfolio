5dbfe637ee1c491e99e49f5c8848d3babddb31cc
renaud dor <renaud.dor@gmail.com>
2020-03-22 10:38:59 +0100

    add order to projects. Create simple index table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:show, :new, :create]
+  resources :projects, only: [:show, :new, :create, :index, :edit]
   resources :contacts, only: :create
 end
