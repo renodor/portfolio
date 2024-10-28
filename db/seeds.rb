c7af7b6e00a8483b57c9e40d786aab354b0a0f08
renodor <renaud.dor@gmail.com>
2024-10-28 12:46:17 +0100

    Design project show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,20 +11,34 @@ p "Destroy all Serivces"
 Service.destroy_all
 p "---------> Done!"
 
+p "Destroy all Technologies"
+Technology.destroy_all
+p "---------> Done!"
+
 p "Destroy all Projects"
 Project.destroy_all
 p "---------> Done!"
-p ""
-p "Create New Projects"
 
+p "Create Services"
 backend_service = Service.create!(name: "Backend web development", icon: "icons/code.svg")
 frontend_service = Service.create!(name: "Frontend web development", icon: "icons/pen.svg")
 
+p "Create Technologies"
+ruby_on_rails_tech = Technology.create!(name: "Ruby on Rails")
+solidus_tech = Technology.create!(name: "Solidus")
+bootstrap_tech = Technology.create!(name: "Bootstrap")
+sidekiq_tech = Technology.create!(name: "Sidekiq")
+sentry_tech = Technology.create!(name: "Sentry")
+cloudinary_tech = Technology.create!(name: "Cloudinary")
+postgres_tech = Technology.create!(name: "PostgreSQL")
+
+p "Create New Projects"
 p1 = Project.new(
   name: "Aliance Francaise",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.",
   url: "www.exemple.com",
-  services: [backend_service, frontend_service]
+  services: [backend_service, frontend_service],
+  technologies: [ruby_on_rails_tech, solidus_tech, bootstrap_tech, sidekiq_tech, sentry_tech, cloudinary_tech, postgres_tech]
 )
 cover_photo = URI.open('https://res.cloudinary.com/detkhu57i/image/upload/v1584630456/portfolio/aliance-francaise_ptwjek.png')
 p1.cover_photo.attach(io: cover_photo, filename: "aliance-francaise.png", content_type: "image/png")
@@ -34,7 +48,8 @@ p2 = Project.new(
   name: "Projet Source",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.",
   url: "www.exemple.com",
-  services: [backend_service, frontend_service]
+  services: [backend_service, frontend_service],
+  technologies: [ruby_on_rails_tech, bootstrap_tech, cloudinary_tech, postgres_tech]
 )
 cover_photo = URI.open('https://res.cloudinary.com/detkhu57i/image/upload/v1584630452/portfolio/project-source_j3cwqi.png')
 p2.cover_photo.attach(io: cover_photo, filename: "projet-source.png", content_type: "image/png")
@@ -44,7 +59,8 @@ p3 = Project.new(
   name: "Line Up Trade",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.",
   url: "www.exemple.com",
-  services: [backend_service, frontend_service]
+  services: [backend_service, frontend_service],
+  technologies: [ruby_on_rails_tech, bootstrap_tech, cloudinary_tech, postgres_tech]
 )
 cover_photo = URI.open('https://res.cloudinary.com/detkhu57i/image/upload/v1584630457/portfolio/lineuptrade_aaxcw9.png')
 p3.cover_photo.attach(io: cover_photo, filename: "lineup-trade.png", content_type: "image/png")
@@ -64,17 +80,26 @@ p5 = Project.new(
   name: "Salva Mi Maquina",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.",
   url: "www.exemple.com",
-  services: [backend_service, frontend_service]
+  services: [backend_service, frontend_service],
+  technologies: [ruby_on_rails_tech, solidus_tech, bootstrap_tech, sidekiq_tech, sentry_tech, cloudinary_tech, postgres_tech]
+
 )
 cover_photo = URI.open('https://res.cloudinary.com/detkhu57i/image/upload/v1584630450/portfolio/salvamimaquina_hkbj2g.png')
 p5.cover_photo.attach(io: cover_photo, filename: "salvamimaquina.png", content_type: "image/png")
+
+photo1 = URI.open('https://res.cloudinary.com/detkhu57i/image/upload/c_fit,h_300/jhcdojdj37llit7rs3i9oxtgsm5r')
+p5.photos.attach(io: photo1, filename: "salvamimaquina1.png", content_type: "image/png")
+
+photo2 = URI.open('https://res.cloudinary.com/detkhu57i/image/upload/c_fit,h_300/v5ewrupbzwfjo9w5qcep4emv3y5w')
+p5.photos.attach(io: photo2, filename: "salvamimaquina1.png", content_type: "image/png")
 p5.save!
 
 p6 = Project.new(
   name: "Bd Troc",
   description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia, architecto necessitatibus minima nesciunt obcaecati ipsa natus.",
   url: "www.exemple.com",
-  services: [backend_service]
+  services: [backend_service],
+  technologies: [postgres_tech, sidekiq_tech]
 )
 cover_photo = URI.open('https://res.cloudinary.com/detkhu57i/image/upload/v1584630460/portfolio/bdtroc_p75geh.png')
 p6.cover_photo.attach(io: cover_photo, filename: "bdtroc.png", content_type: "image/png")
